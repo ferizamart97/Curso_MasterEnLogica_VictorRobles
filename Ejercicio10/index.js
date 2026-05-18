4352e92bc8e457c68f5f0994df67bedc945fa081
EUROMUNDO\fernando.izazaga <fernando.izazaga@euromundo.com.mx>
2026-05-18 13:55:45 -0600

    Ejercicio 10: escalera con numero de escalones.

--- Ejercicio10/index.js
+++ Ejercicio10/index.js
@@ -9,3 +9,27 @@ EJEMPLO
 
 */
 
+function escalones(nivel){
+
+    let numeroEscalones = nivel;
+    let fila = 0;
+    let priamide = "";
+    let escalones = "[-]";
+    
+    for(let i = 0; i < numeroEscalones; i++){
+
+        priamide = priamide + '\n';
+        fila++;
+
+        for(let j = 0; j < fila; j++){
+            priamide += escalones; 
+        }
+
+    }
+
+    return priamide;
+
+}
+
+
+console.log(escalones(10))
