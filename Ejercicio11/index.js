b88ef1c39f2cd4c8267ed869b90a7bce80cf6e9b
EUROMUNDO\fernando.izazaga <fernando.izazaga@euromundo.com.mx>
2026-05-18 17:01:19 -0600

    Ejercicio 11: palabras censuradas

--- Ejercicio11/index.js
+++ Ejercicio11/index.js
@@ -6,3 +6,17 @@ console(funcion("hola - hola", "hola"))
  
 DEVUELVE [-CENSURADO-] O NO PUEDES LEER EL TEXTO Y LA BUSQUEDA
 */
+
+
+function ocultar(frase, palabra){
+
+    let palabraCensura = "[-CENSURADO-]";
+    let textoCensurado = frase.replace(palabra, palabraCensura);
+
+    return textoCensurado; 
+
+}
+
+
+
+console.log(ocultar("Hola, este mensaje es para ver las habilidades en js", "js"))
