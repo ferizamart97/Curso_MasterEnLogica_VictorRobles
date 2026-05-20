1d0a5784174cbdc5fdf95c59fbe4ef63521cbb80
EUROMUNDO\fernando.izazaga <fernando.izazaga@euromundo.com.mx>
2026-05-19 18:49:03 -0600

    Ejercicio 18: Divisores de un numero

--- Ejercicio18/index.js
+++ Ejercicio18/index.js
@@ -1,4 +1,4 @@
-/* DADO UN NUMERO, MOSTRAR TODOS SUS NUMEROS DIVISORES 
+/* DADO UN NUMERO, MOSTRAR TODOS SUS NUMEROS DIVISORES HASTA ESE NUMERO
 
 EJEMPLO
 mostrarDivisores(5):
@@ -7,3 +7,15 @@ DEVUELVE
 1
 5
 */
+
+function divisores(numero){
+    let lista = "";
+    for(let i = 1; i <= numero; i++){
+        if(numero % i === 0){
+            lista += i + '\n';
+        }
+    }
+    return lista;
+}
+
+console.log(divisores(10));
