e65937d798e3cf9b79b5550a4a64cc5ab64a24c8
EUROMUNDO\fernando.izazaga <fernando.izazaga@euromundo.com.mx>
2026-05-19 18:48:40 -0600

    Ejercicio 17: Version FizzBuzz

--- Ejercicio17/index.js
+++ Ejercicio17/index.js
@@ -3,3 +3,27 @@ DADO UN NUMERO, MOSTRAR LOS NUMEROS DE 1 HASTA EL NUMERO.
 PERO PARA MULTIPLOS DE CINCO IMPRIMIR "LIGHTYEAR"
 PARA MULTIPLOS DE TRES Y CINCO "BUZZLIGHTYEAR"
  */
+
+function serie(numero){
+
+    let lista = "";
+
+    for(let i = 1; i <= numero; i++){
+
+
+        if(i % 3 === 0 && i % 5 === 0){
+            lista += "BUZZLIGHTYEAR" + "\n";
+        }else if(i % 3 === 0){
+            lista += "BUZZ" + "\n";
+        }else if(i % 5 === 0){
+            lista += "LIGHTYEAR" + "\n";
+        }else { 
+            lista += i + "\n";
+        }
+
+    }   
+
+    return lista; 
+}
+
+console.log(serie(100));
