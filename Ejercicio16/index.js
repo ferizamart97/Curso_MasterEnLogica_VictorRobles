c4dc63c8b51b79f181aad5e6f676b518a3eae87d
EUROMUNDO\fernando.izazaga <fernando.izazaga@euromundo.com.mx>
2026-05-19 18:48:13 -0600

    Ejercicio 16: Conteo vocales en palabra

--- Ejercicio16/index.js
+++ Ejercicio16/index.js
@@ -4,6 +4,32 @@ EJEMPLO
 vocales("Fernando") 
 
 DEVUELVE
-2
+3
 
 */
+
+function vocales(palabra){
+
+    let arrayVocales = ["a", "e", "i", "o", "u"];
+    let contador = 0;
+
+    for(let i = 0; i < palabra.length; i++){
+
+        let letra = palabra[i].toLowerCase();
+
+        for(let j = 0; j < arrayVocales.length; j++){
+            
+            if(letra === arrayVocales[j]){
+                contador++;
+            }
+
+        }
+
+    }
+
+    return contador;
+
+}
+
+
+console.log(vocales("AeoU"))
