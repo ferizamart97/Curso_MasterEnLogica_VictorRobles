ac3e2b95f367dbb9256fa391127ad52d759170d8
EUROMUNDO\fernando.izazaga <fernando.izazaga@euromundo.com.mx>
2026-05-19 18:47:35 -0600

    Ejercicio 15: letra repetida

--- Ejercicio15/index.js
+++ Ejercicio15/index.js
@@ -7,3 +7,33 @@ DEVUELVE
 lo que mas se repite es: n (Letra que mas se repite)
 
 */
+
+function letraRepetida(palabra){
+    
+    let tabla = {}; 
+    let palabras = palabra.split("");
+    let mayor = 0;
+    let claveMayor = "";
+
+    for(let i = 0; i < palabras.length; i++ ){
+        const letra = palabras[i];
+
+        if(tabla[letra]){
+            tabla[letra] = tabla[letra] + 1;
+        }else{
+            tabla[letra] = 1;
+        }
+    }
+
+    for(const clave in tabla){
+        if(tabla[clave] > mayor){
+            mayor = tabla[clave];
+            claveMayor =  "lo que mas se repite es:" + clave;
+        }
+    }
+
+    return claveMayor;
+    
+}
+
+console.log(letraRepetida("Fernando"))
